e02a067079b0f376ea4d1d36a96634ef521d8a7b
alero <alero@DESKTOP-SQ5BNGD>
2021-12-16 12:08:58 +0000

    A simplified version of blackjack that plays automatically. It reparts cards to 3 players and then to the croupier. All players play against the croupier not against themselves.

--- src/control/BlackJack.java
+++ src/control/BlackJack.java
@@ -1,7 +1,8 @@
 package control;
 
 import java.util.ArrayList;
-import view.Utilities;
+import view.CardUtilities;
+import view.GameUtilities;
 
 
 
@@ -17,22 +18,22 @@ public class BlackJack {
         ArrayList<ArrayList<String>> players;
         ArrayList<String> croupier = new ArrayList<String>();
         
-        deck = Utilities.fillAndMixDeck();
-        players = Utilities.getPlayers(3);
-        Utilities.firstTwoCards(players, deck);
-        Utilities.giveCards(players, deck);
-        Utilities.giveCardsCroupier(croupier, deck);
+        deck = CardUtilities.fillAndMixDeck();
+        players = CardUtilities.getPlayers(3);
+        CardUtilities.firstTwoCards(players, deck);
+        CardUtilities.giveCards(players, deck);
+        CardUtilities.giveCardsCroupier(croupier, deck);
         
         System.out.println("Final results: ");
         for (int i = 0; i < 3; i++) {
             System.out.println("Player "+ (i+1)+": " + players.get(i) + " (" 
-                                + Utilities.checkSum(players.get(i))+ ")");
+                                + GameUtilities.checkSum(players.get(i))+ ")");
         }
         
         System.out.println("Croupier :" + croupier + " (" 
-                                + Utilities.checkSum(croupier)+ ")");
+                                + GameUtilities.checkSum(croupier)+ ")");
         
-        winners = Utilities.getWinners(players.get(0), players.get(1), 
+        winners = GameUtilities.getWinners(players.get(0), players.get(1), 
                                         players.get(2), croupier, deck);
                                         
         for (String winner : winners) {
--- src/view/GameUtilities.java
+++ src/view/GameUtilities.java
@@ -3,60 +3,8 @@ package view;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Utilities {
+public class GameUtilities {
     
-    public static ArrayList<String> fillAndMixDeck(){
-        ArrayList<String> deck = new ArrayList<>();
-        
-        for(int j = 0; j<=3; j++){
-            
-            for(Integer i = 2; i <= 10; i++){
-                deck.add(i.toString());
-            }
-            
-            deck.add("A");
-            deck.add("J");
-            deck.add("Q");
-            deck.add("K");
-        }
-        Collections.shuffle(deck);
-        return deck;
-        
-    }
-    
-    public static ArrayList<ArrayList<String>> getPlayers(int numberOfPlayers){
-        ArrayList<ArrayList<String>> players = new ArrayList<ArrayList<String>>();
-        for(int i = 0; i < numberOfPlayers; i++){
-            players.add(new ArrayList<String>());
-            
-            
-        }
-        
-        return players;
-    }
-    
-    public static void firstTwoCards(ArrayList<ArrayList<String>> players, ArrayList<String> deck){
-        for (ArrayList<String> player : players) {
-            player.add(deck.remove(deck.size()-1));
-            player.add(deck.remove(deck.size()-1)); 
-        }
-        
-        
-    }
-    
-    public static void giveCards (ArrayList<ArrayList<String>> players, ArrayList<String> deck){
-        for (ArrayList<String> player : players) {
-            while(Utilities.checkSum(player) <= 16){ 
-                player.add(deck.remove(deck.size()-1));
-            }
-        }
-    }
-    
-    public static void giveCardsCroupier (ArrayList<String> croupier, ArrayList<String> deck){
-        while(Utilities.checkSum(croupier) <= 16){ 
-                croupier.add(deck.remove(deck.size()-1));
-        }
-    }
     
     public static int checkSum(ArrayList<String> cards) {
         ArrayList<String> player = new ArrayList<String>(cards);
